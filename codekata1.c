9308101e36b2615973ca335300d184346b7d3d05
sharmabhupendra <35636326+sharmabhupendra@users.noreply.github.com>
2018-10-16 22:28:02 +0530

    Create codekata1.c

---
+++ codekata1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+int a[10000],c[10000]={0};
+for(int i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+  c[a[i]]++;
+}
+for(int i=0;i<=10000;i++)
+{
+  if(c[i]>1)
+  printf("%d ",i);
+}
+}
