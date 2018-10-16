8d69392c27135ced8749fe3b0e0d906de5d00bfc
sharmabhupendra <35636326+sharmabhupendra@users.noreply.github.com>
2018-10-16 22:31:24 +0530

    Update codekata1.c

--- codekata1.c
+++ codekata1.c
@@ -9,9 +9,15 @@ for(int i=0;i<n;i++)
   scanf("%d",&a[i]);
   c[a[i]]++;
 }
+  int j=0;
 for(int i=0;i<=10000;i++)
 {
   if(c[i]>1)
-  printf("%d ",i);
+  {
+    printf("%d ",i);
+    j=1;
+  }
 }
+  if(j==1)
+    printf("unique");
 }
